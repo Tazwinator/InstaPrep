72c2781c5b6d1ee3cd1548f63a04af5985c94e9d
Taylor <tazwinator@gmail.com>
2023-08-31 22:50:28 +0100

    Added React-Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import  { Provider } from "react-redux";
 import { store } from "./app/store.ts";
+
+
 import App from './App.tsx'
+import ErrorPage from "./error-page";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-		<App />
+		  <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
